a6b496f5aab5325330c1a7a3ade38ae865aec1a4
Evzen Huml <evzen.huml@zpovednicar.cz>
2022-05-17 12:56:44 +0100

    meta refresh

--- webroot/index.php
+++ webroot/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Zpovednicar</title>
     <link rel="icon" href="data:,">
+    <meta http-equiv="refresh" content="0; url=https://zpovednicar.github.io/">
 </head>
 <body>
 <?= $_SERVER['SERVER_NAME'] ?>
